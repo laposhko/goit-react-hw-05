b9a3893855ce481a1a5e03136eaad24f6d2d1634
laposhko <laposhko0@gmail.com>
2024-04-28 20:57:26 +0300

    added styles

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -7,8 +7,20 @@ export default function MovieList({ movies }) {
       <ul className={css.list}>
         {movies.map((movie) => (
           <li key={movie.id}>
-            <Link to={`/movies/${movie.id}`} state={location}>
+            <Link
+              to={`/movies/${movie.id}`}
+              state={location}
+              className={css.card}
+            >
               {movie.title}
+              <img
+                src={
+                  movie.backdrop_path
+                    ? `https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`
+                    : "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg"
+                }
+                alt={movie.overview}
+              ></img>
             </Link>
           </li>
         ))}
